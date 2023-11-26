28070f953456249cad945c5a72c51f29af968d59
v1centebarros <vmabarros@ua.pt>
2023-11-26 15:36:50 +0000

    Add numbers to the radar chart

--- src/components/plot/spiderPlot/grid.jsx
+++ src/components/plot/spiderPlot/grid.jsx
@@ -16,39 +16,51 @@ export const SpiderGrid = ({
 
         if (angle === undefined) return null;
 
-        const path = lineGenerator([[angle, INNER_RADIUS], [angle, outerRadius],]);
+        const path = lineGenerator([[angle, INNER_RADIUS], [angle, outerRadius]]);
 
         const labelPosition = polarToCartesian(angle - Math.PI / 2, outerRadius + 10);
 
         return (<g key={i}>
-                <path d={path} stroke={GRID_COLOR} strokeWidth={0.5} rx={1}/>
-                <text
-                    x={labelPosition.x}
-                    y={labelPosition.y}
-                    fontSize={12}
-                    fill={GRID_COLOR}
-                    textAnchor={labelPosition.x > 0 ? 'start' : 'end'}
-                    dominantBaseline="middle"
-                >
-                    {axis.name}
-                </text>
-            </g>);
+            <path d={path} stroke={GRID_COLOR} strokeWidth={0.5} rx={1}/>
+            <text
+                x={labelPosition.x}
+                y={labelPosition.y}
+                fontSize={12}
+                fill={GRID_COLOR}
+                textAnchor={labelPosition.x > 0 ? 'start' : 'end'}
+                dominantBaseline="middle"
+            >
+                {axis.name}
+            </text>
+        </g>);
     });
 
-    // Compte grid = concentric circles
+    // Compute grid = concentric circles
     const allCircles = [...Array(GRID_NUMBER).keys()].map((position, i) => {
-        return (<circle
-                key={i}
+        const radius = INNER_RADIUS + (position * (outerRadius - INNER_RADIUS)) / (GRID_NUMBER - 1);
+        return (<g key={i}>
+            <circle
                 cx={0}
                 cy={0}
-                r={INNER_RADIUS + (position * (outerRadius - INNER_RADIUS)) / (GRID_NUMBER - 1)}
+                r={radius}
                 stroke={GRID_COLOR}
                 fill="none"
-            />);
+            />
+            <text
+                x={0}
+                y={-radius - 6}
+                fontSize={10}
+                fill={GRID_COLOR}
+                textAnchor="end"
+                dominantBaseline="middle"
+            >
+                {((axisConfig[0].max / (GRID_NUMBER - 1)) * position).toFixed(0)}
+            </text>
+        </g>);
     });
 
     return (<g>
-            {allAxes}
-            {allCircles}
-        </g>);
+        {allAxes}
+        {allCircles}
+    </g>);
 };
