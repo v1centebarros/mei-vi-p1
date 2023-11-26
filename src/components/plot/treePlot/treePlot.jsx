76819451b5690c325aa9d61c60721c09ee159705
v1centebarros <vmabarros@ua.pt>
2023-11-26 22:40:17 +0000

    revert this shit

--- src/components/plot/treePlot/treePlot.jsx
+++ src/components/plot/treePlot/treePlot.jsx
@@ -80,7 +80,7 @@ export const TreePlot = ({data, width, height, margin, regionName}) => {
             })
             // Make div disappear
             .on("mouseout", function () {
-                return tooltip.style("visibility", "hidden").style("top", "0px").style("left", "0px");
+                return tooltip.style("visibility", "hidden")
             });
 
         // Adding text labels only if they fit in the rectangle
